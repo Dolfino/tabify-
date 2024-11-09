4dab3a2cf6aabf24fde03afadac8a4e4d480ed0b
David N. da Silva <davidnascimentodasilva@hotmail.com>
2024-11-09 15:45:09 +0000

    Adicionado script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabify-
+
 Uma extensão de navegador leve ou utilitário para agilizar o gerenciamento de guias, fornecendo recursos como agrupamento de guias, pesquisa e troca rápida.
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,30 @@
 function Home() {
-    return (
-        <div>
-            <h1>Bem-vindo à Minha Landing Page</h1>
-            <p>Aqui você encontra soluções para todos os seus desafios.</p>
-            <p>Nossa missão é proporcionar qualidade e excelência em tudo o que fazemos.</p>
-            <p>Entre em contato e descubra como podemos ajudar você a alcançar o sucesso.</p>
-            
-            <ul>
-                <li>Solução personalizada para suas necessidades</li>
-                <li>Equipe qualificada e dedicada</li>
-                <li>Atendimento 24/7</li>
-            </ul>
+  return (
+    <div>
+      <h1>Bem-vindo à Minha Landing Page</h1>
+      <p>Aqui você encontra soluções para todos os seus desafios.</p>
+      <p>
+        Nossa missão é proporcionar qualidade e excelência em tudo o que
+        fazemos.
+      </p>
+      <p>
+        Entre em contato e descubra como podemos ajudar você a alcançar o
+        sucesso.
+      </p>
 
-            <button onClick={() => alert("Obrigado por visitar!")}>Saiba Mais</button>
+      <ul>
+        <li>Solução personalizada para suas necessidades</li>
+        <li>Equipe qualificada e dedicada</li>
+        <li>Atendimento 24/7</li>
+      </ul>
 
-            <footer>
-                <p>&copy; 2024 Minha Landing Page. Todos os direitos reservados.</p>
-            </footer>
-        </div>
-    );
+      <button onClick={() => alert("Obrigado por visitar!")}>Saiba Mais</button>
+
+      <footer>
+        <p>&copy; 2024 Minha Landing Page. Todos os direitos reservados.</p>
+      </footer>
+    </div>
+  );
 }
 
 export default Home;
